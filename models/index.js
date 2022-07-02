4fa0569680a089b920408c2c752438d338980fc8
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-07-02 23:18:04 +0100

    models

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  onDelete: "CASCADE",
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  onDelete: "CASCADE",
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(User, {
+  onDelete: "CASCADE",
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  onDelete: "CASCADE",
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
